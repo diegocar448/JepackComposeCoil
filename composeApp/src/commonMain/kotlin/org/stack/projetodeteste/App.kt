734c5b8164f5a258af1c60c28d0f02be638df771
Diego Cardoso <diegocar448@hotmail.com>
2024-12-19 15:20:23 -0300

    finalizado

--- composeApp/src/commonMain/kotlin/org/stack/projetodeteste/App.kt
+++ composeApp/src/commonMain/kotlin/org/stack/projetodeteste/App.kt
@@ -2,21 +2,31 @@ package org.stack.projetodeteste
 
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil3.ImageLoader
+import coil3.compose.AsyncImage
+import coil3.compose.setSingletonImageLoaderFactory
+import coil3.request.crossfade
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.stack.projetodeteste.collections.PhotoList
 import org.stack.projetodeteste.model.Photo
@@ -32,6 +42,11 @@ fun App() {
         photo.getPhotos { list ->
             photoList = list
         }
+    }
+
+    //manter as imagens carregadas em cache
+    setSingletonImageLoaderFactory { context ->
+        ImageLoader.Builder(context).crossfade(true).build()
     }
 
 
@@ -51,7 +66,16 @@ fun App() {
             contentPadding = PaddingValues(10.dp)
         ){
             itemsIndexed(photoList){ position, photo ->
+                AsyncImage(
+                    model = photo.url,
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.fillMaxWidth().padding(5.dp).clip(
+                        RoundedCornerShape(15.dp)
+                    ).aspectRatio(2 / 3f).clickable {
 
+                    }
+                )
             }
         }
     }
--- composeApp/src/commonMain/kotlin/org/stack/projetodeteste/collections/PhotoList.kt
+++ composeApp/src/commonMain/kotlin/org/stack/projetodeteste/collections/PhotoList.kt
@@ -43,6 +43,42 @@ class PhotoList {
             ),
             Photo(
                 url = "https://img.freepik.com/fotos-gratis/vista-de-gatos-e-caes-mostrando-amizade_23-2151806280.jpg"
+            ),
+            Photo(
+                url = "https://img.freepik.com/fotos-premium/jaguar-perseguindo-na-grama-close-de-um-jaguar_372999-9989.jpg?w360"
+            ),
+            Photo(
+                url = "https://avesornamentaisjej.cdn.magazord.com.br/img/2024/03/produto/488/84-tucano-toco-3.png?ims=600x600"
+            ),
+            Photo(
+                url = "https://designi.com.br/images/preview/11452593.jpg"
+            ),
+            Photo(
+                url = "https://img.freepik.com/fotos-premium/pinguinos-nadando-mar_960396-583952.jpg"
+            ),
+            Photo(
+                url = "https://almadeviajante.com/wp-content/uploads/veneza-grand-canal-1024x682.jpg"
+            ),
+            Photo(
+                url = "https://www.machupicchupacotes.com/wp-content/uploads/2023/08/machu-picchu-peru.webp"
+            ),
+            Photo(
+                url = "https://img.freepik.com/fotos-premium/as-piramides-de-gize-no-deserto-do-egito_400112-811.jpg"
+            ),
+            Photo(
+                url = "https://www.papeldeparede.etc.br/fotos/wp-content/uploads/1440_white-winter-wallpaper.jpeg"
+            ),
+            Photo(
+                url = "https://certifiedhumanebrasil.org/wp-content/uploads/2018/10/Galinhas-Poedeiras.jpg"
+            ),
+            Photo(
+                url = "https://img.freepik.com/fotos-gratis/vista-de-um-luxuoso-navio-de-cruzeiro_23-2150785234.jpg"
+            ),
+            Photo(
+                url = "https://img.freepik.com/fotos-gratis/estadio-de-futebol-cheio-de-gente_23-2151548530.jpg"
+            ),
+            Photo(
+                url = "https://img.freepik.com/fotos-gratis/vista-de-gatos-e-caes-mostrando-amizade_23-2151806280.jpg"
             )
         )
         response(photoList)
